13edaf646379223f0b909e8c05480157ea5c4df2
jatin <jatin.ahluwalia.5@gmail.com>
2023-11-11 16:58:15 +0530

    get api

--- app/api/shiprocket/route.ts
+++ app/api/shiprocket/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
 
 export async function POST(request: any) {
-    return NextResponse.json('Hello World')
+  return NextResponse.json("Hello World");
+}
+export async function GET(request: any) {
+  return NextResponse.json("Hello World");
 }
